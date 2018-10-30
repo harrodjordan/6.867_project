53e813761dd853e0c731b42bc1f9231e70f2baf7
Jordan Harrod <jordanharrod@MacBook-Pro-9.local>
2018-10-30 15:45:41 -0400

    initial project files

---
+++ models.py
@@ -0,0 +1,41 @@
+import sklearn 
+import keras.layers as k
+import matplotlib.pyplot as plt 
+import numpy 
+import os 
+
+class RNN():
+
+	def __init__():
+		pass
+
+	def train():
+		pass
+
+	def test():
+		pass
+
+
+class SVM():
+
+	def __init__():
+		pass
+
+	def train():
+		pass
+
+	def test():
+		pass
+
+
+
+class RandomForest():
+
+	def __init__():
+		pass
+
+	def train():
+		pass
+
+	def test():
+		pass
---
+++ project.py
@@ -0,0 +1,6 @@
+import models 
+import keras
+import sklearn 
+import os
+import matplotlib.pyplot as plt 
+

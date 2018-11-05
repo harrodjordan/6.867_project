c3020fcf71cda04111bd9c75271ac1c1351a724d
daphneschles <daph@jhu.edu>
2018-11-04 22:14:16 -0500

    filling in the rf classifier

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ import keras.layers as k
 import matplotlib.pyplot as plt 
 import numpy 
 import os 
+from sklearn.ensemble import RandomForestClassifier
 
 class RNN():
 
@@ -28,14 +29,22 @@ class SVM():
 		pass
 
 
-
-class RandomForest():
+# TODO add to parameter list
+class RandomForest(n_estimators=10):
 
 	def __init__():
-		pass
+		# initialize classifier
+		self.clf = RandomForestClassifier(n_estimators=n_estimators)
 
-	def train():
-		pass
+	def train(X,Y):
+		# fit the classifier
+		self.clf = clf.fit(X,Y)
 
-	def test():
-		pass
+	# do we want to just spit out labels or testing accuracies?
+	# kind of an arbitrary choice
+	# we also care about things like TPR etc
+	def test(X, true_labels):
+		# predict labels
+		prediction = self.clf.predict(X)
+		# get mean accuracy (not a good metric!)
+		score = self.clf.score(X, true_labels)

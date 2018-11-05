40dc422f59ae961ac354e465f0c8e1dbbe596235
Jordan Harrod <jordanharrod@MacBook-Pro-9.local>
2018-11-05 10:51:45 -0500

    model updates

--- models.py
+++ models.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import numpy 
 import os 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+import pywt
+
 
 class RNN():
 
@@ -19,31 +22,39 @@ class RNN():
 
 class SVM():
 
-	def __init__():
-		pass
+	def __init__(self, C=0.001):
+        # initialize classifier
+        clf = svm.LinearSVC(C=C)
+		
 
 	def train():
-		pass
-
+        # fit the classifier
+        X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2)
+        self.clf = self.clf.fit(X,Y)
+        
 	def test():
-		pass
+        prediction = self.clf.predict(X)
+        # get mean accuracy (not a good metric!)
+        score = self.clf.score(X, true_labels)
+
 
 
 # TODO add to parameter list
 class RandomForest(n_estimators=10):
 
-	def __init__():
+	def __init__(self):
 		# initialize classifier
 		self.clf = RandomForestClassifier(n_estimators=n_estimators)
 
-	def train(X,Y):
+	def train(self,X,Y):
 		# fit the classifier
-		self.clf = clf.fit(X,Y)
+        X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2)
+		self.clf = self.clf.fit(X,Y)
 
 	# do we want to just spit out labels or testing accuracies?
 	# kind of an arbitrary choice
 	# we also care about things like TPR etc
-	def test(X, true_labels):
+	def test(self,X, true_labels):
 		# predict labels
 		prediction = self.clf.predict(X)
 		# get mean accuracy (not a good metric!)

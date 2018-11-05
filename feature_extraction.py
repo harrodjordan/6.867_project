68afaa479721a6a0ce36b3c495cc8c3b7e36135b
daphneschles <daph@jhu.edu>
2018-11-04 21:52:41 -0500

    initial script for feature extraction. Just returns labels, raw data

---
+++ feature_extraction.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+#from matplotlib import pyplot as plt
+
+def raw_data() :
+
+	N = 11500
+	d = 180
+
+	df = pd.read_csv('data.csv', sep=',', header=None, low_memory=False)
+
+	dat = df.values
+
+	name_list = dat[:,0]
+
+	dat_vals = dat[1:,1:].astype(np.float)
+
+	return name_list, dat_vals
+
+# sp = np.fft.fft(dat_vals)
+# freq = np.fft.fftfreq(dat_vals.shape[-1])
+
+# plt.plot(freq, sp[1,:].real)
+# plt.show()

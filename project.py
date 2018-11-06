f2938a3d7bb91acb44fc851b892488a8c68e6986
daphneschles <daph@jhu.edu>
2018-11-06 18:18:33 -0500

    printing cross val score

--- project.py
+++ project.py
@@ -4,6 +4,8 @@ import sklearn
 import os
 import matplotlib.pyplot as plt 
 import feature_extraction
+from sklearn.model_selection import cross_val_score
+import numpy as np
 
 # fun fact exactly 20% of the samples are seizure data
 
@@ -13,4 +15,7 @@ rf = models.RandomForest()
 rf.train(data, labels)
 sc = rf.test(data, labels)
 
+cvsc = np.mean(cross_val_score(rf.clf, data, labels, cv=10))
+
 print('Random Forest Accuracy: ' + str(sc))
+print('Random Forst Cross Validation Score: ' + str(cvsc))

a194d54f3676354ba2a4f78cf7f93bfc09d2a129
daphneschles <daph@jhu.edu>
2018-11-06 17:59:26 -0500

    cleaner output

--- project.py
+++ project.py
@@ -5,11 +5,12 @@ import os
 import matplotlib.pyplot as plt 
 import feature_extraction
 
+# fun fact exactly 20% of the samples are seizure data
+
 data, labels, name_list = feature_extraction.raw_data(two_cat = True)
 
 rf = models.RandomForest()
 rf.train(data, labels)
 sc = rf.test(data, labels)
 
-print(sc)
-print(name_list)
+print('Random Forest Accuracy: ' + str(sc))

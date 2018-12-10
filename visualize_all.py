664a6c69b9679ad2d0ad2ccf902f93770c922058
Daphne Schlesinger <daphne@Daphnes-MacBook-Pro.local>
2018-12-10 15:57:51 -0500

    tiny syntax error

--- visualize_all.py
+++ visualize_all.py
@@ -9,7 +9,7 @@ from sklearn.decomposition import PCA
 
 data, labels, name_list = feature_extraction.raw_data(two_cat = True)
 
-X_train, X_test, y_train, y_test = train_test_split(data, labels, name_list, test_size = 0.2)
+#X_train, X_test, y_train, y_test = train_test_split(data, labels, name_list, test_size = 0.2)
 
 pca = PCA()
 
@@ -23,14 +23,14 @@ plt.scatter(avges,sigs,alpha=0.5,c=labels)
 plt.xlabel('mean')
 plt.ylabel('stdev')
 plt.title('True labels w.r.t. mean and stdev of each sample')
-plt.show()
+#plt.show()
 
 plt.figure()
 plt.scatter(data_pca[:,0],data_pca[:,1],alpha=0.5,c=labels)
 plt.xlabel('pca_1')
 plt.ylabel('pca_2')
 plt.title('True labels in principle component space')
-plt.show()
+#plt.show()
 
 # now, plot the curves
 t = np.linspace(0,1000,data.shape[1])
@@ -43,5 +43,6 @@ for i in range(data.shape[0]) :
 
 plt.xlabel('time (ms)')
 plt.ylabel('signal')
-plt.title('True labels of signals')
-plt.show()
+#plt.title('True labels of signals')
+#plt.show()
+plt.savefig('sig_labels_all.png')
